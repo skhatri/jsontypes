003bf2f0eff9125145876223d2e02589ee0b8d16
Suresh Khatri <servlet2@msn.com>
2013-07-12 16:43:30 +1000

    Update TypeTest.java

--- test/json/TypeTest.java
+++ test/json/TypeTest.java
@@ -1,5 +1,6 @@
 package json;
 
+import org.joda.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,17 +14,25 @@ public class TypeTest {
         String specialSerialized = JSON.stringify(special);
         Assert.assertTrue(specialSerialized.contains("SpecialTask"));
 
+        LocalDate now = new LocalDate();
+
         GenericTask generic = new GenericTask();
+        generic.setCreatedDate(now);
         generic.setName("khatri");
 
         String genericSerialized = JSON.stringify(generic);
 
+        String todayAsString = now.toString("dd/MM/yyyy");
         Assert.assertTrue(genericSerialized.contains("GenericTask"));
+        Assert.assertTrue(genericSerialized.contains(todayAsString));
 
         SpecialTask anotherSpecial = JSON.parse(specialSerialized, SpecialTask.class);
+        anotherSpecial.setCreatedDate(new LocalDate());
         Assert.assertEquals("skhatri", anotherSpecial.getOperator());
+
 
         GenericTask anotherGeneric = JSON.parse(genericSerialized, GenericTask.class);
         Assert.assertEquals("khatri", anotherGeneric.getName());
+
     }
 }
